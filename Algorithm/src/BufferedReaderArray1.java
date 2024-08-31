bb50bdba1d8529051fbccb89b13be756e873304f
Ana-hyang <anahyang@gmail.com>
2024-08-31 14:02:06 +0900

    한줄로 들어온 br을 배열로 받는 법

--- Algorithm/src/BufferedReaderArray1.java
+++ Algorithm/src/BufferedReaderArray1.java
@@ -1,19 +1,19 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Test {
+public class BufferedReaderArray1 {
 
 	public static void main(String[] args) throws IOException {
-		// 선언 및 입력받기
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		String str = br.readLine();
-		String arr [] = new String [3];
+		String arr[] = new String[3];
 		arr = str.split(" ");
-		
-		System.out.println(arr[0]);
-		System.out.println(arr[1]);
-		System.out.println(arr[2]);
+
+		System.out.println(arr[0] + " " + arr[1] + " " + arr[2]);
 	}
+
 }
