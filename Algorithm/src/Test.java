e4574c40397bfaad480774543a0291d22d15eea1
Ana-hyang <anahyang@gmail.com>
2024-08-31 13:30:11 +0900

    프로젝트 추가

---
+++ Algorithm/src/Test.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// 선언 및 입력받기
+		System.out.println("HE");
+
+		// 받은 문자열을 줄이기
+
+		// 새로운 문자열을 알파벳 배열로 확인하기
+
+		// 출력하기
+	}
+}
