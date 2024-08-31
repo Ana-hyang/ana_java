7d66eef9454206238dc06c5cf639b2e7c812bf9f
Ana-hyang <anahyang@gmail.com>
2024-08-31 14:10:59 +0900

    여러 줄을 br로 입력받아 배열에 저장하는 법

--- Algorithm/src/BufferedReaderArray1.java
+++ Algorithm/src/BufferedReaderArray1.java
@@ -5,7 +5,8 @@ import java.io.InputStreamReader;
 public class BufferedReaderArray1 {
 
 	public static void main(String[] args) throws IOException {
-
+		// 1차원 배열 입력
+		// 1 2 3
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
--- Algorithm/src/BufferedReaderArray2.java
+++ Algorithm/src/BufferedReaderArray2.java
@@ -1,9 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class BufferedReaderArray2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		// 1차원 배열 입력
+		// 1
+		// 2
+		// 3
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+		String arr[] = new String[3];
+		for(int i =0 ; i<3 ; i++) {
+			arr[i] = br.readLine();
+		}
+		
+
+		System.out.println(arr[0] + " " + arr[1] + " " + arr[2]);
 	}
 
 }
